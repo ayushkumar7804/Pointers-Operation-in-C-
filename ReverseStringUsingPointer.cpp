6332d904c8949fe5d6df7c7e6fdcd225f360ea09
KassArc <ayushkumaryadav2029@gmail.com>
2025-08-19 21:57:53 +0530

    Create ReverseStringUsingPointer.cpp

---
+++ ReverseStringUsingPointer.cpp
@@ -0,0 +1,27 @@
+// Ayush Yadav
+// 24070123028
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1;
+    cout << "Enter the string to reverse\n";
+    cin >> str1;
+
+    int n = str1.length();
+    char* ptr = &str1[0];  
+
+    cout << "Reversed String\n";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << *(ptr + i);  
+    }
+
+    return 0;
+}
+
+// OUTPUT
+// Enter the string to reverse
+// Ayush
+// Reversed String
+// hsuyA
